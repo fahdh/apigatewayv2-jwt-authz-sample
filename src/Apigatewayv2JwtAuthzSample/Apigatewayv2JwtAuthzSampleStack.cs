e5dfeb2d0eba7400aae33945beb133e694bb8ec8
Fahd Hamidani <fahd_hamidani@hotmail.com>
2020-06-02 09:56:38 -0700

    Initial commit

---
+++ src/Apigatewayv2JwtAuthzSample/Apigatewayv2JwtAuthzSampleStack.cs
@@ -0,0 +1,12 @@
+using Amazon.CDK;
+
+namespace Apigatewayv2JwtAuthzSample
+{
+    public class Apigatewayv2JwtAuthzSampleStack : Stack
+    {
+        internal Apigatewayv2JwtAuthzSampleStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
---
+++ src/Apigatewayv2JwtAuthzSample/Program.cs
@@ -0,0 +1,17 @@
+﻿using Amazon.CDK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Apigatewayv2JwtAuthzSample
+{
+    sealed class Program
+    {
+        public static void Main(string[] args)
+        {
+            var app = new App();
+            new Apigatewayv2JwtAuthzSampleStack(app, "Apigatewayv2JwtAuthzSampleStack");
+            app.Synth();
+        }
+    }
+}
